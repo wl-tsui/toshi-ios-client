99a61f0a2322b07305e137082c78069d26654170
Igor Ranieri Elland <igor@elland.me>
2017-01-13 13:06:04 +0100

    Added missing user session setup on user creation.

--- Token/idAPIClient.swift
+++ Token/idAPIClient.swift
@@ -23,18 +23,26 @@ public class IDAPIClient: NSObject {
     
     public func registerUserIfNeeded(username: String? = nil, name: String? = nil) {
         self.retrieveUser(username: username ?? self.cereal.address) { user in
-            guard user == nil else { return }
+            guard user == nil else {
+                User.current = user
+
+                return
+            }
 
             let userParameters = UserIDRegistrationParameters(name: name, username: username, cereal: self.cereal)
             self.networking.POST("/v1/user", parameterType: .json, parameters: userParameters.signedParameters()) { (json, error) in
-                print(json ?? "")
-                print(error ?? "")
+                if let error = error {
+                    print(error)
+                } else if let json = json as? [String: Any] {
+                    User.current = User(json: json)
+                } else {
+                    fatalError()
+                }
             }
         }
     }
 
     public func retrieveUser(username: String, completion: @escaping((User?) -> Void)) {
-        var user: User?
         self.networking.GET("/v1/user/\(username)/") { json, error in
             if let error = error {
                 print(error)
