a7521b2c48bfbbec7f403fe77eb2509ab5861992
Ellen Shapiro <designatednerd@gmail.com>
2017-12-08 15:23:02 +0100

    Add skeleton methods for various things, set up match.

---
+++ fastlane/toshi.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+# Information specific to the Toshi app
+class Toshi
+
+  # The username for an Apple account that can log in to the Apple Developer portal and iTunes Connect
+  Username = "ios@bakkenbaeck.com"
+
+  class TeamId
+    # The Apple developer portal team ID for development/distribution via App Store
+    Developer = "BH32JXCQWU"
+    # The Apple developer portal team ID for Enterprise distribution
+    Enterprise = "3TAZ984YN3"
+  end
+
+  # The names of build schemes available for build actions
+  class BuildScheme
+    Tests = "Tests"
+    Debug = "Debug"
+    Development = "Development"
+    Distribution = "Distribution"
+  end
+
+  # Information specific to this app's branch and repo for Match.
+  class MatchInfo
+    RepoUrl = "https://github.com/bakkenbaeck/fastlane-match.git"
+    Branch = "toshi"
+  end
+
+  # All bundle identifiers for this application and its assorted extensions.
+  class BundleIdentifier
+    Debug = "org.toshi.debug"
+    Development = "no.bakkenbaeck.toshi.enterprise"
+    Distribution = "org.toshi.distribution"
+  end
+
+end
