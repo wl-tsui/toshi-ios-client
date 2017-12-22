4d19b70728b1b7c6848cef6cac2d9b270af78400
Igor Ranieri Elland <Elland@users.noreply.github.com>
2017-12-22 09:48:08 +0100

    Fixed issue with regex and string handling for username detection. (#647)
    * Removed duplicate from xcodeproj. Bumped Xcode version.
    
    * No need to match case-sensitive if we're doing a case-insensitive regex.
    
    * Fixed text range calculation.
    
    * Safe-guard against string live manipulation issues.
    
    * Prevent matching usernames inside URLs with @ signs. Added tests. (#648)
    
    * Prevent matching usernames inside URLs with @ signs.
    
    * Extracted the username detection method.
    
    Due to the cell architecture, some stuff broke, so I had to do some changes here and there.
    
    * Fixed text colours.
    
    * Added basic tests to string transformer.
    
    * Extracted the username detection method.
    
    Due to the cell architecture, some stuff broke, so I had to do some changes here and there.
    
    * Fixed text colours.
    
    * Added basic tests to string transformer.
    
    * Fixed typo.
    
    * Updated test to check for username inside link.

--- Toshi/MessagesUI/Cells/MessagesTextCell.swift
+++ Toshi/MessagesUI/Cells/MessagesTextCell.swift
@@ -8,14 +8,19 @@ class MessagesTextCell: MessagesBasicCell {
 
     var messageText: String? {
         didSet {
-            textView.text = messageText
-            detectUsernameLinksIfNeeded()
-
             if let messageText = messageText, messageText.hasEmojiOnly, messageText.emojiVisibleLength <= 3 {
+                textView.text = messageText
                 bubbleView.backgroundColor = nil
                 textView.font = Theme.emoji()
+            } else {
+                let usernameLinkColor = isOutGoing ? Theme.lightTextColor : Theme.tintColor
+                textView.attributedText = TextTransformer.attributedUsernameString(to: messageText, textColor: messageColor, linkColor: usernameLinkColor, font: Theme.preferredRegularText())
             }
         }
+    }
+
+    private var messageColor: UIColor {
+        return isOutGoing ? .white : .black
     }
 
     private lazy var textView: UITextView = {
@@ -31,6 +36,7 @@ class MessagesTextCell: MessagesBasicCell {
         view.textContainerInset = .zero
         view.textContainer.lineFragmentPadding = 0
         view.textContainer.maximumNumberOfLines = 0
+
         view.linkTextAttributes = [NSAttributedStringKey.underlineStyle.rawValue: NSUnderlineStyle.styleSingle.rawValue]
 
         return view
@@ -49,14 +55,6 @@ class MessagesTextCell: MessagesBasicCell {
         super.showSentError(show, animated: animated)
         textView.isUserInteractionEnabled = !show
     }
-
-    private lazy var usernameDetector: NSRegularExpression = {
-        do {
-            return try NSRegularExpression(pattern: " ?(@[a-zA-Z][a-zA-Z0-9_]{2,59}) ?", options: [.caseInsensitive, .useUnicodeWordBoundaries])
-        } catch {
-            fatalError("Couldn't instantiate usernameDetector, invalid pattern for regular expression")
-        }
-    }()
 
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -85,32 +83,5 @@ class MessagesTextCell: MessagesBasicCell {
         textView.font = Theme.preferredRegularText()
         textView.adjustsFontForContentSizeCategory = true
         textView.text = nil
-    }
-
-    private func detectUsernameLinksIfNeeded() {
-        guard frame.isEmpty == false else { return }
-
-        if let text = textView.attributedText?.mutableCopy() as? NSMutableAttributedString {
-            let range = NSRange(location: 0, length: text.string.length)
-            
-            text.addAttributes([.kern: -0.4], range: range)
-
-            usernameDetector.enumerateMatches(in: text.string, options: [], range: range) { [weak self] result, _, _ in
-
-                guard let strongSelf = self else { return }
-
-                if let result = result {
-                    let attributes: [NSAttributedStringKey: Any] = [
-                        .link: "toshi://username:\((text.string as NSString).substring(with: result.range(at: 1)))",
-                        .foregroundColor: (strongSelf.isOutGoing ? Theme.lightTextColor : Theme.tintColor),
-                        .underlineStyle: NSUnderlineStyle.styleSingle.rawValue
-                    ]
-
-                    text.addAttributes(attributes, range: result.range(at: 1))
-                }
-            }
-
-            textView.attributedText = text
-        }
     }
 }
