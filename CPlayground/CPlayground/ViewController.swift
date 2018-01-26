e762f24aad350d34cfc91ad6ada0ee10b433ad7f
Ellen Shapiro <designatednerd@gmail.com>
2018-01-26 12:54:15 +0100

    add base cplaygorund project

---
+++ CPlayground/CPlayground/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CPlayground
+//
+//  Created by Ellen Shapiro (Work) on 1/24/18.
+//  Copyright © 2018 Toshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
